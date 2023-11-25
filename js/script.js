fce095b6e4f724852539f6298e26a1defc9d1dd9
Satveer <84339533+satveerkaur@users.noreply.github.com>
2023-11-25 16:57:02 -0700

    Add function to play one round

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
 function getComputerChoice(){
     let choices= ['Rock','Paper','Scissor'];
     let random = Math.floor(Math.random()*3);
-    let computerChoice = choices[random];
+    let computerChoice = choices[random].toLowerCase();
     return computerChoice;
 }
-let computerChoice = getComputerChoice().toLowerCase();
+let computerChoice = getComputerChoice();
 let playerSelection = prompt("Rock, paper or scissor");
-
+playerSelection = playerSelection.toLowerCase();
 function play(playerSelection,computerChoice){
-    if(playerSelection === )
+    if(playerSelection === 'rock' && computerChoice === 'paper'){
+        console.log("Computer wins!");
+    }
+    else if(playerSelection === 'rock' && computerChoice === 'scissor'){
+        console.log('Player wins!');
+    }
+    else if(playerSelection === 'paper' && computerChoice === 'rock'){
+        console.log('Player wins');
+    }
+    else if(playerSelection === 'paper' && computerChoice === 'scissor'){
+        console.log('Computer wins!');
+    }
+    else if(playerSelection === 'scissor' && computerChoice === 'rock'){
+        console.log('computer wins');
+    }
+    else if(playerSelection === 'scissor' && computerChoice === 'paper'){
+        console.log('player wins');
+    }else{
+        console.log("It's a tie");
+    }
 }
+
+play(playerSelection,computerChoice);
+
